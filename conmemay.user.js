1c18ee13561da8e3432db598d249600ad7b4f49b
neeuloveu <neeiyeuem@gmail.com>
2026-05-17 11:05:39 +0700

    Fix UserScript header syntax

---
+++ conmemay.user.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name         Bypass UptoLink
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  Bypass UptoLink for cuttay user
+// @require      https://github.com/phatnottaken51/bypass-upto-link/raw/refs/heads/main/source.js
+// @match        *://*/*
+// @author       PhatNotTaken
+// @grant        GM_xmlhttpRequest
+// @connect      uptolink.one
+// @connect      raw.githubusercontent.com
+// @connect      api.github.com
+// @connect      generativelanguage.googleapis.com
+// @connect      api.kolosal.ai
+// @connect      *
+// @cun-at       document-end
+// @icon         https://cdn.discordapp.com/avatars/1001691686721835090/8c9770820eac0449217292ed718b3700.png
+// ==/UserScript==
